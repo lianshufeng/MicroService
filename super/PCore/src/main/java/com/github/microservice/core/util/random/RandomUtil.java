0a1c4faa44ac1d7604138458390a9c5afa4853de
lianshufeng <251708339@qq.com>
2024-04-25 19:46:44 +0800

    submit

---
+++ super/PCore/src/main/java/com/github/microservice/core/util/random/RandomUtil.java
@@ -0,0 +1,20 @@
+package com.github.microservice.core.util.random;
+
+import java.util.Random;
+
+public class RandomUtil {
+
+
+    /**
+     * 取指定返回的随机数
+     *
+     * @param min
+     * @param max
+     * @return
+     */
+    public int nextInt(int min, int max) {
+        return new Random().nextInt((max - min) + 1) + min;
+    }
+
+
+}
